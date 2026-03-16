113035599face7f42c2145eb7f612ed289080ac1
Rivaldo Roberto <rrs.sistema@gmail.com>
2026-03-16 20:34:04 -0300

    Finalizando atividade 001

--- app_Loop_02/App.js
+++ app_Loop_02/App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
 
-export default function App() {
+function App() {
+
+  const jogadores = [
+    "Goleiro-------: Alisson",
+    "Lateral Dir.--: - Danilo",
+    "Zagueiro------: - Marquinhos",
+    "Zagueiro------: - Thiago Silva",
+    "Lateral Esq.--: - Alex Sandro",
+    "Volante-------: - Casemiro",
+    "Meia----------: - Lucas Paquetá",
+    "Meia----------: - Neymar",
+    "Ponta Dir.----: - Raphinha",
+    "Centroavante--: - Richarlison",
+    "Ponta Esq-----:. - Vinícius Jr"
+  ];
+
+  let elementos = null;
+
+  for (let i = 0; i < jogadores.length; i++) {
+    elementos = (
+      <>
+        {elementos}
+
+        <View
+          style={{
+            backgroundColor: "rgba(30,136,229,0.9)",
+            marginVertical: 6,
+            padding: 12,
+            borderRadius: 10,
+            width: 280,
+            elevation: 5
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontSize: 16,
+              fontWeight: "bold",
+              textAlign: "center"
+            }}
+          >
+            {i + 1} ⚽ {jogadores[i]}
+          </Text>
+        </View>
+
+      </>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/bandeira-brasil.jpg')}
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+      }}
+      resizeMode="cover"
+    >
+      <Text
+        style={{
+          fontSize: 28,
+          fontWeight: "bold",
+          color: "white",
+          marginBottom: 20,
+          textShadowColor: "black",
+          textShadowOffset: { width: 2, height: 2 },
+          textShadowRadius: 5
+        }}
+      >
+        🇧🇷 Escalação da Seleção
+      </Text>
+
+      {elementos}
+
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
