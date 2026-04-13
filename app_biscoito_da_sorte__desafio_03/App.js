33b103cc6d55a396323771a60585acd3474b4117
Rivaldo Roberto <rrs.sistema@gmail.com>
2026-04-13 17:37:50 -0300

    App-BiscoitoDaSorte Finalizando Desafio 03

---
+++ app_biscoito_da_sorte__desafio_03/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
+import { estilos } from './src/styleSheet/estilos';
+
+function App() {
+  const ArrayFrases = [
+    'Siga as boas pessoas e aprenda com elas',
+    'O bom-senso vale mais do que muito conhecimento',
+    'O riso é a maior distância entre duas pessoas',
+    'Deixe de lado as preocupações e seja feliz',
+    'Realize o óbvio, pense no improvável e conquiste o impossível',
+    'Acredite em milagres, mas não dependa deles',
+    'A maior barreira para o sucesso é o medo do fracasso',
+  ];
+
+  const [textoBotao, setTextoBotao] = useState('Quebrar Biscoito!');
+  const [imgBiscoito, setImgBiscoito] = useState(
+    require('./src/img/BS02_biscoito.png')
+  );
+
+  function alteraStatusBiscoito() {
+    window.alert('oi');
+  }
+
+  return (
+    <View style={estilos.areaFundo}>
+      <View style={estilos.areaTitulo}>
+        <Text style={estilos.titulo}>App Biscoito da Sorte</Text>
+      </View>
+
+      <View style={estilos.areaImg}>
+        <Image source={imgBiscoito} style={estilos.img} />
+      </View>
+
+      <View>
+        <Text style={estilos.textoFrase}>Algumas frases aqui.....</Text>
+
+        <TouchableOpacity style={estilos.botao} onPress={alteraStatusBiscoito}>
+          <View style={estilos.areaBotao}>
+            <Image
+              source={require('./src/img/BS03_Btn_icone_01.png')}
+              style={{ marginRight: 20, width: 40, height: 40 }}
+            />
+            <Text style={estilos.textoBotao}>{textoBotao}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default App;
